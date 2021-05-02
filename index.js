00abe2aebe3a4c1f3af077d15fd2255c955e583a
abhishek.mohata12@gmail.com <ab-phoenix>
2021-05-02 15:56:15 +0530

    Update @hapi/joi to joi and remove defaultMessage var and set extra condition for default message and added new test case for it

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var assert = require('assert');
-var defaultMessage = 'valid mongo id';
 
 module.exports = function joiObjectId(Joi, message) {
   assert(Joi && Joi.object, 'you must pass Joi as an argument');
-  if (message == undefined) {
-    message = defaultMessage;
+  if (!message || !(typeof message === 'string')) {
+    message = 'valid mongo id';
   }
   return function objectId() {
     return Joi.string().regex(/^[0-9a-fA-F]{24}$/, message);
